6ba7f79d938a08e33c8b6eb8b67a3d4efc944f03
Zipzapadam <amchatila@gmail.com>
2015-08-01 17:28:40 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -14,12 +14,12 @@ exports.port = 8000;
 //   of strings. Each string should be either an IP address or a subnet given
 //   in CIDR notation. You should usually leave this as `false` unless you
 //   know what you are doing.
-exports.proxyip = false;
+exports.proxyip = true;
 
 // Pokemon of the Day - put a pokemon's name here to make it Pokemon of the Day
 //   The PotD will always be in the #2 slot (not #1 so it won't be a lead)
 //   in every Random Battle team.
-exports.potd = '';
+exports.potd = 'Bidoof';
 
 // crash guard - write errors to log file instead of crashing
 //   This is normally not recommended - if Node wants to crash, the
